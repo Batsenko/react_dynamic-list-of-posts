7f7c7efe5b00519fceba4af7ade0df2f21f8fb8b
unknown <batsenkod@gmail.com>
2019-06-12 13:16:40 +0400

    init

---
+++ precache-manifest.5a274de95770c2b42b8fc9edcdc2bfe3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab0ee8d2f62ab3e079137528c793b5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b3c6bf25953745f8e2c8",
+    "url": "./static/css/main.3fc2d845.chunk.css"
+  },
+  {
+    "revision": "0c4d940c47b748e59b1c",
+    "url": "./static/js/2.06399952.chunk.js"
+  },
+  {
+    "revision": "b3c6bf25953745f8e2c8",
+    "url": "./static/js/main.8848327c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
